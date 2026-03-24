30302594f1b54f9cc815e54642695d4fcc69796f
clicon-fanpage <beniteznegocios@gmail.com>
2026-03-24 10:35:58 -0300

    Update productoscatalogo.js

--- catalogo/productoscatalogo.js
+++ catalogo/productoscatalogo.js
@@ -13,21 +13,21 @@ const productosCatalogo = [
     nombre: "Signos",
     precio: 7000,
     categoria: "signos",
-    imagen: "./img.catalogo/signos1.png",
+    imagen: "catalogo/img.catalogo/signos1.png",
     galeria: generarGaleria("signos", 4)
   },
   {
     nombre: "Anime",
     precio: 7000,
     categoria: "anime",
-    imagen: "./img.catalogo/anime1.png",
+    imagen: "catalogo/img.catalogo/anime1.png",
     galeria: generarGaleria("anime", 4)
   },
   {
     nombre: "Planetas",
     precio: 7000,
     categoria: "personalizado",
-    imagen: "./img.catalogo/planetas1.png",
+    imagen: "catalogo/img.catalogo/planetas1.png",
     galeria: generarGaleria("planetas", 4)
   }
 ];
